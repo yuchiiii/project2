52e9000aa6f65b318dca041e7b3aa8ba85c4f34e
Yuchi Hsieh <hsiehy8@uw.edu>
2020-12-17 05:20:36 -0800

    add my_knn_cv vignettes and edit my_knn_cv

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -23,29 +23,31 @@ my_knn_cv <- function(train, cl, k_nn, k_cv) {
   fold <- sample(rep(1:k_cv, length = nrow(train)))
 
   # Create a new data frame storing the folds.
-  train_new <- cbind(train, fold)
+  # train_new <- cbind(train, fold)
 
   # Empty vector to store predictions and prediction error.
   pred <- vector()
   class <- vector()
 
+  data_train <- dplyr::select(train, bill_length_mm, bill_depth_mm,flipper_length_mm, body_mass_g)
+  data_test <- dplyr::select(train, bill_length_mm, bill_depth_mm,flipper_length_mm, body_mass_g)
+
   # Do k-nearest neighbors cross-validation and calculate the misclassification rate for each iteration.
   for (i in 1:k_cv) {
-    data_train <- dplyr::filter(train_new, fold != i)
-    data_train <- dplyr::select(data_train, bill_length_mm, bill_depth_mm,flipper_length_mm, body_mass_g)
-    data_test <-  dplyr::filter(train_new, fold == i)
-    data_test <- dplyr::select(data_test, bill_length_mm, bill_depth_mm,flipper_length_mm, body_mass_g)
+    data_train_cv <- dplyr::filter(data_train, fold != i)
+    data_test_cv <-  dplyr::filter(data_train, fold == i)
     cl_train <- cl[fold != i]
     cl_test <- cl[fold == i]
-    prediction <- class::knn(data_train, data_test, cl_train, k = k_nn)
+    prediction <- class::knn(data_train_cv, data_test_cv, cl_train, k = k_nn)
     prediction <- as.character(prediction)
-    class[fold == i] <- prediction
     pred_err <- sum(cl_test != prediction) / length(cl_test)
     pred[fold == i] <- pred_err
   }
+  all_pred <- class::knn(data_train, data_test, cl, k = k_nn)
+  all_pred <- as.character(all_pred)
   cv_error <- mean(pred)
 
   # Create a list to store the cv_error and all predictions.
-  result <- list("class" = class, "cv_err" = cv_error)
+  result <- list("class" = all_pred, "cv_err" = cv_error)
   return(result)
 }
